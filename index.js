c80605b36c8f65216f66a47ec891a0da6e6adcdb
mhellar <mark.hellar@gmail.com>
2017-08-20 23:03:19 -0700

    execute git commands

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 var githubhook = require('githubhook');
 var github = githubhook({ /* options */ });
+var exec = require('child_process').exec;
 
 github.listen();
 
 github.on('*', function(event, repo, ref, data) {});
+
+github.on('push', function(event, repo, ref, data) {
+    console.log(req.body.pusher.name + ' just pushed to ' + data.repository.full_name);
+
+    console.log('pulling code from GitHub...');
+
+    // reset any changes that have been made locally
+    exec('git -C ~/githook_handler reset --hard', execCallback);
+
+    // and ditch any files that have been added locally too
+    exec('git -C ~/githook_handler clean -df', execCallback);
+
+    // now pull down the latest
+    exec('git -C ~/githook_handler pull -f', execCallback);
+
+    // and npm install with --production
+    exec('npm -C ~/githook_handler install --production', execCallback);
+
+    // and run tsc
+    exec('tsc', execCallback);
+});
 
 github.on('event', function(repo, ref, data) {});
 
